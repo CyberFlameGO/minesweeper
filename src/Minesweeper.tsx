72a06cb7ac46bd9328f04055390ed1fcb2c6a944
kris10ansn <telefon.kristian@gmail.com>
2021-03-13 01:12:51 +0100

    add customization (refactors needed

--- src/Minesweeper.tsx
+++ src/Minesweeper.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Cell from "./Cell";
 import {
     ICell,
@@ -19,9 +19,16 @@ const WIDTH = 16;
 const HEIGHT = 16;
 
 const Minesweeper: React.FC = () => {
-    const [gameState, setGameState] = useState<number>(State.NOT_STARTED);
+    const [width, setWidth] = useState(16);
+    const [height, setHeight] = useState(16);
+    const [mines, setMines] = useState(64);
 
-    const [board, setBoard] = useState(baseBoard(WIDTH, HEIGHT));
+    const [gameState, setGameState] = useState<number>(State.NOT_STARTED);
+    const [board, setBoard] = useState(() => baseBoard(WIDTH, HEIGHT));
+
+    useEffect(() => {
+        setBoard(baseBoard(width, height));
+    }, [width, height]);
 
     const handleClick = (x: number, y: number, cell: ICell) => (
         event: React.MouseEvent
@@ -32,9 +39,9 @@ const Minesweeper: React.FC = () => {
         if (gameState === State.NOT_STARTED) {
             if (leftClick) {
                 const newBoard = setupBoard({
-                    width: WIDTH,
-                    height: HEIGHT,
-                    mines: (WIDTH * HEIGHT) / 4,
+                    width,
+                    height,
+                    mines,
                     click: { x, y },
                 });
 
@@ -95,6 +102,44 @@ const Minesweeper: React.FC = () => {
                     </div>
                 ))}
             </div>
+
+            <label>
+                Width
+                <input
+                    type="range"
+                    min={1}
+                    max={32}
+                    value={width}
+                    onInput={({ target }) =>
+                        setWidth(Number((target as HTMLInputElement).value))
+                    }
+                />
+            </label>
+            <label>
+                Height
+                <input
+                    type="range"
+                    min={1}
+                    max={32}
+                    value={height}
+                    onInput={({ target }) =>
+                        setHeight(Number((target as HTMLInputElement).value))
+                    }
+                />
+            </label>
+
+            <label>
+                Mines
+                <input
+                    type="number"
+                    min={1}
+                    max={500}
+                    value={mines}
+                    onInput={({ target }) =>
+                        setMines(Number((target as HTMLInputElement).value))
+                    }
+                />
+            </label>
 
             {gameState === State.WIN && <h1>You won!</h1>}
             {gameState === State.LOSS && <h1>You lost!</h1>}
