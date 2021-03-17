98ce2aa537bdf13de3ef0033859dfc730251e688
kris10ansn <telefon.kristian@gmail.com>
2021-03-17 22:49:35 +0100

    use Cell, component, log clicks

--- src/Minesweeper.tsx
+++ src/Minesweeper.tsx
@@ -3,18 +3,27 @@ import { array2d } from "./util/array";
 import { addBombs, calculateValues } from "./util/board";
 import { pipe } from "./util/functions";
 import "./Minesweeper.scss";
+import Cell from "./Cell";
 
 const Minesweeper: React.FC<{}> = () => {
     const [board] = useState(() =>
         pipe(array2d(16)(16)(0), addBombs(20), calculateValues)
     );
 
+    const handleClick = (x: number, y: number) => () => {
+        console.log(x, y);
+    };
+
     return (
         <table id="Minesweeper">
-            {board.map((row) => (
-                <tr>
-                    {row.map((x) => (
-                        <td>{x}</td>
+            {board.map((row, y) => (
+                <tr key={y}>
+                    {row.map((value, x) => (
+                        <Cell
+                            value={value}
+                            onClick={handleClick(x, y)}
+                            key={x}
+                        />
                     ))}
                 </tr>
             ))}
