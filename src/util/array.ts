117109347dd092c7776f0bab6ec93f00b6c03d62
kris10ansn <telefon.kristian@gmail.com>
2021-03-17 22:06:36 +0100

    add types

--- src/util/array.ts
+++ src/util/array.ts
@@ -1,3 +1,13 @@
+export type Length<T extends any[]> = T extends { length: infer L } ? L : never;
+export type DropFirst<T extends any[]> = ((...args: T) => any) extends (
+    arg: any,
+    ...rest: infer U
+) => any
+    ? U
+    : T;
+
+export type LastElement<T extends any[]> = T[Length<DropFirst<T>>];
+
 export const array = (length: number) => Array<null>(length).fill(null);
 
 export const array2d = (width: number) => (height: number) => <T>(
