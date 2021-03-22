a6324685242826e9cc352685d3819115e36e37c0
kris10ansn <telefon.kristian@gmail.com>
2021-03-22 17:00:33 +0100

    encode both storage name and value

--- src/util/useStoredState.ts
+++ src/util/useStoredState.ts
@@ -1,24 +1,28 @@
 import React, { useEffect, useState } from "react";
 
-const stringify = (obj: any) => btoa(JSON.stringify(obj));
-const parse = (json: string) => JSON.parse(atob(json));
+const encode = (data: string) => btoa(data);
+const decode = (data: string) => atob(data);
+
+const stringify = (obj: any) => encode(JSON.stringify(obj));
+const parse = (json: string) => JSON.parse(decode(json));
 
 export const useStoredState = <T>(
     name: string,
     initialValue: T | (() => T)
 ): [T, React.Dispatch<React.SetStateAction<T>>, () => void] => {
-    const stored = localStorage.getItem(name);
+    const storedName = encode(name);
+    const stored = localStorage.getItem(storedName);
 
     const [state, setState] = useState<T>(
         stored ? parse(stored) : initialValue
     );
 
     useEffect(() => {
-        localStorage.setItem(name, stringify(state));
-    }, [name, state]);
+        localStorage.setItem(storedName, stringify(state));
+    }, [storedName, state]);
 
     const clearState = () => {
-        localStorage.removeItem(name);
+        localStorage.removeItem(storedName);
     };
 
     return [state, setState, clearState];
