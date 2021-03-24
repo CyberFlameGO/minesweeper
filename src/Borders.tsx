318c14929fffd4f7b6982d6b9d8f129cfbb56d26
kris10ansn <telefon.kristian@gmail.com>
2021-03-25 00:41:44 +0100

    add settings ribbon

--- src/Borders.tsx
+++ src/Borders.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Borders.scss";
+import settings from "./assets/settings-white.svg";
 
 const Borders: React.FC = ({ children }) => {
     return (
         <div className="Borders">
+            <div className="ribbon">
+                <img src={settings} alt="settings" />
+            </div>
             <div className="content">{children}</div>
         </div>
     );
