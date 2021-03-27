e1b3d44f0f83b98b238b22e91ef196e93d72ca61
kris10ansn <telefon.kristian@gmail.com>
2021-03-27 18:49:17 +0100

    use SettingsOverlay

--- src/Minesweeper.tsx
+++ src/Minesweeper.tsx
@@ -20,6 +20,7 @@ import Cell from "./Cell";
 import match from "./util/functions/match";
 import Borders from "./Borders";
 import { useStoredState } from "./util/useStoredState";
+import SettingsOverlay from "./SettingsOverlay";
 
 enum State {
     NOT_STARTED,
@@ -179,6 +180,8 @@ const Minesweeper: React.FC<{}> = () => {
 
     return (
         <Borders>
+            <SettingsOverlay />
+
             <table
                 id="Minesweeper"
                 onContextMenu={preventDefault}
