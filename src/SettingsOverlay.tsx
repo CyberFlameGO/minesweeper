d77ab8ea4edd75b976fa35936efff001188bb358
kris10ansn <telefon.kristian@gmail.com>
2021-03-29 17:19:55 +0200

    update

--- src/SettingsOverlay.tsx
+++ src/SettingsOverlay.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import settings from "./assets/settings-white.svg";
 
 import "./SettingsOverlay.scss";
+import { className } from "./util/functions";
 
 interface SettingsOverlayProps {}
 
 const SettingsOverlay: React.FC<SettingsOverlayProps> = () => {
+    const [open, setOpen] = useState(false);
+
     return (
         <>
-            <div className="ribbon">
-                <img src={settings} alt="settings" />
+            <div className={className({ overlay: true, open })}>
+                <div className="ribbon" onClick={() => setOpen((x) => !x)}>
+                    <img src={settings} alt="settings" />
+                </div>
             </div>
         </>
     );
