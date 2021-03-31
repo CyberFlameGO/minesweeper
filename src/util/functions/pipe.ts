e5e762754e77199ab13efc4137a21e7c12767175
kris10ansn <telefon.kristian@gmail.com>
2021-03-31 22:39:42 +0200

    add fallback type

--- src/util/functions/pipe.ts
+++ src/util/functions/pipe.ts
@@ -3,7 +3,9 @@ import { LastElement } from "../array";
 type Func = (arg: any) => any;
 type ArgType<F, Else = never> = F extends (arg: infer A) => any ? A : Else;
 
-export const pipe = <F extends [Func, ...Func[]]>(...functions: F) => (
+export const pipe = <F extends [Func, ...Func[]]>(...functions: F | Func[]) => (
     arg: ArgType<F[0]>
-): ReturnType<LastElement<F>> =>
-    functions.reduce((prev, cur) => cur(prev), arg);
+) =>
+    functions.reduce((prev, cur) => cur(prev), arg) as ReturnType<
+        LastElement<F>
+    >;
