bba8ae230416ef438b8726eec046b1ef9126f7f0
kris10ansn <telefon.kristian@gmail.com>
2021-03-31 22:26:25 +0200

    add better types

--- src/util/functions/pipe.ts
+++ src/util/functions/pipe.ts
@@ -1,8 +1,20 @@
 import { LastElement } from "../array";
 
-type F<T> = (arg: T, ...args: any) => any;
+// Types: https://stackoverflow.com/a/53175538/
 
-export const pipe = <T>(...functions: Array<F<T>>) => (
-    arg: T
-): ReturnType<LastElement<typeof functions>> =>
+type Lookup<T, K extends keyof any, Else = never> = K extends keyof T
+    ? T[K]
+    : Else;
+
+type Tail<T extends any[]> = T extends [any, ...infer R] ? R : never;
+
+type Func = (arg: any) => any;
+type ArgType<F, Else = never> = F extends (arg: infer A) => any ? A : Else;
+type AsChain<F extends [Func, ...Func[]], G extends Func[] = Tail<F>> = {
+    [K in keyof F]: (arg: ArgType<F[K]>) => ArgType<Lookup<G, K, any>, any>;
+};
+
+export const pipe = <F extends [Func, ...Func[]]>(
+    ...functions: F & AsChain<F>
+) => (arg: ArgType<F[0]>): ReturnType<LastElement<F>> =>
     functions.reduce((prev, cur) => cur(prev), arg);
