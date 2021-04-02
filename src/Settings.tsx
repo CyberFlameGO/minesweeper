8ce3f2125ccc076475eda39b2a963137006b48b9
kris10ansn <telefon.kristian@gmail.com>
2021-04-02 16:46:18 +0200

    add Settings component

---
+++ src/Settings.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Settings.scss";
+
+interface SettingsProps {}
+
+const Settings: React.FC<SettingsProps> = () => {
+    return <div className="Settings"></div>;
+};
+
+export default Settings;
