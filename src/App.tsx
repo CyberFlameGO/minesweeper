655b2e94aa9254d7ee75c743e75d292575eeef18
kris10ansn <telefon.kristian@gmail.com>
2021-04-12 12:12:23 +0200

    reimplement setting of history if in game

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    useHistory,
+} from "react-router-dom";
 import Minesweeper from "./Minesweeper";
 import MainMenu from "./MainMenu";
 import Settings from "./Settings";
+import { getStoredState } from "./util/useStoredState";
+
+const SetLocation = () => {
+    const history = useHistory();
+    const gameState = getStoredState("state");
+
+    if (gameState !== null) {
+        history.push("/game");
+    }
+
+    return <></>;
+};
+
 const App: React.FC = () => {
     return (
         <Router>
+            <SetLocation />
             <Switch>
                 <Route path="/game">
                     <Minesweeper />
