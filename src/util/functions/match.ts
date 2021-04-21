949d3da94852774b1a3ce6414fb54a6d7dadfdd4
kris10ansn <telefon.kristian@gmail.com>
2021-04-21 22:41:48 +0200

    allow passing of args

--- src/util/functions/match.ts
+++ src/util/functions/match.ts
@@ -4,18 +4,22 @@ const isConditionFunction = <T>(
     c: T | ConditionFunction<T>
 ): c is ConditionFunction<T> => typeof c === "function";
 
-interface MatchObject<T, F = (arg: T) => any> {
-    on: (condition: T | ConditionFunction<T>, fn: F) => MatchObject<T>;
-    otherwise: (fn: F) => any;
+interface MatchObject<T> {
+    on: (
+        condition: T | ConditionFunction<T>,
+        fn: Function,
+        ...args: any[]
+    ) => MatchObject<T>;
+    otherwise: (fn: Function) => any;
 }
 
 const match = <T>(x: T): MatchObject<T> => ({
-    on: (condition, fn) => {
+    on: (condition, fn, ...args) => {
         if (
             (isConditionFunction(condition) && condition(x)) ||
             x === condition
         ) {
-            fn(x);
+            fn(x, ...args);
         }
 
         return match(x);
