9beacf457293679e25c12fdd83c2ee1eb667a666
kris10ansn <telefon.kristian@gmail.com>
2021-04-21 21:35:19 +0200

    restart

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,81 +1,9 @@
 import React from "react";
-import bombImage from "./assets/bomb.png";
-import flag from "./assets/flag.svg";
-import notBomb from "./assets/not-bomb.png";
-import { GameContext, GameState } from "./Minesweeper";
-import { ActionType, createIsActionType } from "./util/action";
-import { isBomb } from "./util/board";
-import { className } from "./util/functions";
-import match from "./util/functions/match";
-import { Range } from "./util/types";
 
-interface CellProps {
-    context: GameContext;
-    x: number;
-    y: number;
-    onLeftClick: React.EventHandler<React.MouseEvent>;
-    onRightClick: React.EventHandler<React.MouseEvent>;
-    onMiddleClick: React.EventHandler<React.MouseEvent>;
-}
+interface CellProps {}
 
-const COLORS = {
-    1: "#0000FF",
-    2: "#007B00",
-    3: "#FF0000",
-    4: "#00007B",
-    5: "#7B0000",
-    6: "#007B7B",
-    7: "#000000",
-    8: "#7B7B7B",
-};
-
-const Cell: React.FC<CellProps> = ({
-    context,
-    x,
-    y,
-    onLeftClick,
-    onRightClick,
-    onMiddleClick,
-}) => {
-    const value = context.board[y][x];
-
-    const is = createIsActionType(context.actions);
-    const isOpen = is(ActionType.OPEN);
-    const isFlagged = is(ActionType.FLAG);
-
-    const color = value > 0 ? COLORS[value as Range<1, 9>] : "";
-    const bomb = isBomb(x, y)(context.board);
-
-    const flagged = isFlagged(x, y);
-    const lost = context.state === GameState.LOST;
-    const open =
-        isOpen(x, y) || (lost && ((flagged && !bomb) || (!flagged && bomb)));
-    const red = bomb && lost && open;
-
-    const mouseDown = (event: React.MouseEvent) =>
-        match(event)
-            .on((e) => e.button === 2, onRightClick)
-            .on((e) => e.button === 1, onMiddleClick);
-
-    const mouseUp = (event: React.MouseEvent) =>
-        event.button === 0 && onLeftClick(event);
-
-    return (
-        <td
-            className={className({ open, red, flagged })}
-            style={open ? { color } : {}}
-            onMouseDown={mouseDown}
-            onMouseUp={mouseUp}
-        >
-            {!(lost && !bomb) && !open && flagged && (
-                <img src={flag} alt="flag" />
-            )}
-            {open && value > 0 && value}
-
-            {lost && flagged && !bomb && <img src={notBomb} alt="not bomb" />}
-            {lost && !flagged && bomb && <img src={bombImage} alt="bomb" />}
-        </td>
-    );
+const Cell: React.FC<CellProps> = () => {
+    return <td></td>;
 };
 
 export default Cell;
--- src/Minesweeper.tsx
+++ src/Minesweeper.tsx
@@ -1,206 +1,25 @@
-import React, { useCallback, useEffect, useMemo } from "react";
-import { addIfNotNull, count } from "./util/array";
-import {
-    createClearClick,
-    openFactory,
-    isBomb,
-    generateBoard,
-    surroundingSquares,
-} from "./util/board";
-import { className, either, preventDefault } from "./util/functions";
-import "./Minesweeper.scss";
-import Cell from "./Cell";
-import match from "./util/functions/match";
-import Borders from "./Borders";
+import React from "react";
+import { array2d } from "./util/array";
 import { getStoredState, useStoredState } from "./util/storedState";
-import {
-    Action,
-    actionFactory,
-    ActionType,
-    allBombsFlagged,
-    createIsActionType,
-    createRemoveAction,
-} from "./util/action";
 
-export enum GameState {
-    NOT_STARTED,
-    STARTED,
-    WON,
-    LOST,
+export interface BoardCell {
+    value: number;
+    open: boolean;
+    flagged: boolean;
 }
 
-export interface GameContext {
-    board: number[][];
-    actions: Action[];
-    state: GameState;
-}
+interface MinesweeperProps {}
 
-const Minesweeper: React.FC<{}> = () => {
+const Minesweeper: React.FC<MinesweeperProps> = () => {
     const width = getStoredState("width", 16);
     const height = getStoredState("height", 16);
     const mines = getStoredState("mines", 15);
 
-    const [board, setBoard, clearStoredBoard] = useStoredState(
-        "board",
-        generateBoard(width, height, mines)
+    const [board, setBoard, clearStoredBoard] = useStoredState("board", () =>
+        array2d(width, height, { value: 0, open: false, flagged: false })
     );
 
-    const [actions, setActions, clearStoredActions] = useStoredState<Action[]>(
-        "actions",
-        []
-    );
-
-    const [state, setState, clearStoredState] = useStoredState(
-        "state",
-        GameState.NOT_STARTED
-    );
-
-    const clearStorage = useCallback(() => {
-        clearStoredBoard();
-        clearStoredActions();
-        clearStoredState();
-    }, [clearStoredActions, clearStoredBoard, clearStoredState]);
-
-    const context: GameContext = useMemo(
-        () => ({
-            board,
-            setBoard,
-            actions,
-            setActions,
-            state,
-            setState,
-        }),
-        [actions, board, setActions, setBoard, setState, state]
-    );
-
-    useEffect(() => {
-        match(state).on(
-            either<GameState>(GameState.LOST, GameState.WON),
-            clearStorage
-        );
-    }, [state, clearStorage]);
-
-    useEffect(() => {
-        if (allBombsFlagged(board, actions)) setState(GameState.WON);
-    }, [actions, board, setState]);
-
-    const is = useMemo(() => createIsActionType(actions), [actions]);
-    const isFlagged = useMemo(() => is(ActionType.FLAG), [is]);
-    const isOpen = useMemo(() => is(ActionType.OPEN), [is]);
-
-    const createLeftClickHandler = useCallback(
-        (x: number, y: number) => () => {
-            const newActions: Array<Action> = [];
-            const open = openFactory(board, actions);
-
-            match(state)
-                .on(GameState.NOT_STARTED, () => {
-                    setBoard(createClearClick(board, 1)(x, y));
-                    setState(GameState.STARTED);
-                })
-                .on(
-                    either<GameState>(GameState.STARTED, GameState.NOT_STARTED),
-                    () => {
-                        if (!isFlagged(x, y)) {
-                            newActions.push(...open(x, y));
-
-                            if (isBomb(x, y)(board)) {
-                                setState(GameState.LOST);
-                            }
-                        }
-                    }
-                );
-
-            const dontOpenFlagged = ({ x, y, type }: Action) =>
-                type === ActionType.OPEN && !isFlagged(x, y);
-
-            setActions([...actions, ...newActions.filter(dontOpenFlagged)]);
-        },
-        [actions, board, isFlagged, setActions, setBoard, setState, state]
-    );
-
-    const createRightClickHandler = useCallback(
-        (x: number, y: number) => () => {
-            const createFlagAction = actionFactory(actions, ActionType.FLAG);
-
-            match(state)
-                .on(GameState.NOT_STARTED, () => {
-                    const leftClick = createLeftClickHandler(x, y);
-                    leftClick();
-                })
-                .on(GameState.STARTED, () => {
-                    if (isFlagged(x, y)) {
-                        const removeFlag = createRemoveAction(ActionType.FLAG);
-                        setActions(removeFlag(actions, x, y));
-                    } else if (!isOpen(x, y)) {
-                        setActions(
-                            addIfNotNull(actions, createFlagAction({ x, y }))
-                        );
-                    }
-                });
-        },
-        [actions, createLeftClickHandler, isFlagged, isOpen, setActions, state]
-    );
-
-    const createMiddleClickHandler = useCallback(
-        (x: number, y: number) => () => {
-            const newActions: Array<Action> = [];
-            const open = openFactory(board, actions);
-
-            if (isOpen(x, y)) {
-                const surrounding = surroundingSquares(board)(x, y);
-
-                const flagged = count(surrounding)(({ x, y }) =>
-                    isFlagged(x, y)
-                );
-
-                if (flagged === board[y][x]) {
-                    surrounding
-                        .filter(({ x, y }) => !isOpen(x, y) && !isFlagged(x, y))
-                        .forEach(({ x, y }) => {
-                            if (isBomb(x, y)(board)) {
-                                setState(GameState.LOST);
-                            }
-                            newActions.push(...open(x, y));
-                        });
-                }
-            }
-
-            setActions([...actions, ...newActions]);
-        },
-        [actions, board, isFlagged, isOpen, setActions, setState]
-    );
-
-    return (
-        <Borders>
-            <table
-                id="Minesweeper"
-                onContextMenu={preventDefault}
-                className={className({ lost: state === GameState.LOST })}
-            >
-                <tbody>
-                    {board.map((row, y) => (
-                        <tr key={y}>
-                            {row.map((value, x) => (
-                                <Cell
-                                    context={context}
-                                    x={x}
-                                    y={y}
-                                    onLeftClick={createLeftClickHandler(x, y)}
-                                    onRightClick={createRightClickHandler(x, y)}
-                                    onMiddleClick={createMiddleClickHandler(
-                                        x,
-                                        y
-                                    )}
-                                    key={x}
-                                />
-                            ))}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </Borders>
-    );
+    return <table></table>;
 };
 
 export default Minesweeper;
