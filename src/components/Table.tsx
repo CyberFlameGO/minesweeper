c0f6b6d2d2d97e3135801afd710663d373815c2f
kris10ansn <telefon.kristian@gmail.com>
2021-04-21 21:47:05 +0200

    add props support

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Table: React.FC = ({ children }) => {
+const Table: React.FC = ({ children, ...props }) => {
     return (
-        <table>
+        <table {...props}>
             <tbody>{children}</tbody>
         </table>
     );
