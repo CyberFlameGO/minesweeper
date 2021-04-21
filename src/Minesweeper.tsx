dee3efa60370c464a72055aba7c05ecf14793301
kris10ansn <telefon.kristian@gmail.com>
2021-04-21 21:37:14 +0200

    use Table wrapper instead of table

--- src/Minesweeper.tsx
+++ src/Minesweeper.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Table from "./components/Table";
 import { array2d } from "./util/array";
 import { getStoredState, useStoredState } from "./util/storedState";
 
@@ -19,7 +20,7 @@ const Minesweeper: React.FC<MinesweeperProps> = () => {
         array2d(width, height, { value: 0, open: false, flagged: false })
     );
 
-    return <table></table>;
+    return <Table></Table>;
 };
 
 export default Minesweeper;
