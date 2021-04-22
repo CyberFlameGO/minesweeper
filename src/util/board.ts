1830b3af0af0913db60ab04d736164b45fedf502
kris10ansn <telefon.kristian@gmail.com>
2021-04-22 10:43:44 +0200

    add surroundingSquares and calculateValues functions

--- src/util/board.ts
+++ src/util/board.ts
@@ -1,4 +1,6 @@
 import { BoardCell } from "../Minesweeper";
+import { area, flatten } from "./array";
+import { createFilter } from "./functions";
 
 export const addBombsPercent = (percent: number) => (board: BoardCell[][]) =>
     board.map((row) =>
@@ -8,3 +10,32 @@ export const addBombsPercent = (percent: number) => (board: BoardCell[][]) =>
                 : { ...values }
         )
     );
+
+export const surroundingSquares = (
+    board: BoardCell[][],
+    cx: number,
+    cy: number
+): BoardCell[] =>
+    flatten(area(board)(cx - 1, cy - 1, cx + 1, cy + 1))
+        .filter(
+            createFilter(
+                ({ x, y }) => x !== cx || y !== cy,
+                ({ value }) => value !== undefined
+            )
+        )
+        .map(({ value }) => value);
+
+const calculateValue = (board: BoardCell[][], x: number, y: number) =>
+    board[y][x].value === -1
+        ? -1
+        : surroundingSquares(board, x, y).filter(({ value }) => value === -1)
+              .length;
+
+export const calculateValues = (board: BoardCell[][]) => {
+    return board.map((row, y) =>
+        row.map(({ ...values }, x) => ({
+            ...values,
+            value: calculateValue(board, x, y),
+        }))
+    );
+};
